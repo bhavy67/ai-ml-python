b8f727ec355f71ff2915deb672dc3d94bf399434
bhavy67 <bhavy.ladani6701@gmail.com>
2025-10-27 21:32:31 +0530

    pass fail

---
+++ Bootcamp/Module_2/pass_or_fail.py
@@ -0,0 +1,12 @@
+# Taking input
+section1 = int(input())
+section2 = int(input())
+section3 = int(input())
+
+# Write your code here
+if section1 >= 30 and section2 >= 30 and section3 >= 30:
+    result = "Pass"
+else:
+    result = "Fail"
+
+print(result)
